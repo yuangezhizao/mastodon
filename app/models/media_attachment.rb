2a8600d3a538f3fb35adcdd35560608f0fbc5940
yuangezhizao-MacMini <yuangezhizao@icloud.com>
2022-01-08 22:21:11 +0800

    ♻️  Refactoring(/app/models/media_attachment.rb): 不再使用 ImageMagick 的 "-quality 90" 参数
    参照：https://github.com/yuangezhizao/mastodon/commit/80c44ed9c19855d494ab26011dca0f6fbc644d0c

--- app/models/media_attachment.rb
+++ app/models/media_attachment.rb
@@ -156,7 +156,7 @@ class MediaAttachment < ApplicationRecord
   }.freeze
 
   GLOBAL_CONVERT_OPTIONS = {
-    all: '-quality 90 -strip +set modify-date +set create-date',
+    all: '-strip +set modify-date +set create-date', #TODO：210108 remove "-quality 90"
   }.freeze
 
   IMAGE_LIMIT = ((ENV['IMAGE_LIMIT'] || 10).to_i).megabytes
