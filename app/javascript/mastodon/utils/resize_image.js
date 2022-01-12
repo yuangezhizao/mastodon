ae6f5d3fc351082e553501ffa76a3327a19cb11a
yuangezhizao-MacMini <yuangezhizao@icloud.com>
2022-01-12 22:58:25 +0800

    ♻️  Refactoring(/app/javascript/mastodon/utils/resize_image.js): 图片像素大小限制修改为 2K 即 2560*1440

--- app/javascript/mastodon/utils/resize_image.js
+++ app/javascript/mastodon/utils/resize_image.js
@@ -1,6 +1,6 @@
 import EXIF from 'exif-js';
 
-const MAX_IMAGE_PIXELS = 48000000; // 1920x1080px #TODO: 210112 change from 2073600 to 48000000
+const MAX_IMAGE_PIXELS = 3686400; // 1920x1080px #TODO: 210112 change from 2073600 to 3686400
 
 const _browser_quirks = {};
 
