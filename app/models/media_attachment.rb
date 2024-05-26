7c9ec85d1c85eb7301f421259156c99551781043
yuangezhizao-MacMini <yuangezhizao@icloud.com>
2024-05-26 17:08:14 +0800

    🔧 Config(.env.production.sample): 「媒体附件」：图片和视频大小限制提取至配置文件
    参照：https://github.com/mastodon/mastodon/pull/23726
    
    (cherry picked from commit 9817d2a3310b43b489fe93c8da31fd6ee5b49089)
    (cherry picked from commit e69c3a9b38215d97fba876ac4286c1c82bb2c86c)
    (cherry picked from commit 41df02b4df02b9b521ed8d308af775a09f5ff618)
    (cherry picked from commit eaed58ef3177acd8f9b714a96d0fbbc8cd3ba312)
    (cherry picked from commit e1bb5d5a37bcbba2529bcfd88bcf6bed9c4a61c9)
    (cherry picked from commit 8681df6da7d5cc76d2b72dd82851b5d130cd622e)
    (cherry picked from commit c18b9d45d207a1862886d380eb9b195411eecc9f)
    (cherry picked from commit 1506c3032365d5dbb7fe75184cf29cfd03b5c490)

--- app/models/media_attachment.rb
+++ app/models/media_attachment.rb
@@ -39,12 +39,12 @@ class MediaAttachment < ApplicationRecord
 
   MAX_DESCRIPTION_LENGTH = 1_500
 
-  IMAGE_LIMIT = 16.megabytes
-  VIDEO_LIMIT = 99.megabytes
+  IMAGE_LIMIT = ((ENV['MEDIAATTACHMENT_IMAGE_LIMIT'] || 16).to_i).megabytes
+  VIDEO_LIMIT = ((ENV['MEDIAATTACHMENT_VIDEO_LIMIT'] || 99).to_i).megabytes
 
-  MAX_VIDEO_MATRIX_LIMIT = 8_294_400 # 3840x2160px
-  MAX_VIDEO_FRAME_RATE   = 120
-  MAX_VIDEO_FRAMES       = 36_000 # Approx. 5 minutes at 120 fps
+  MAX_VIDEO_MATRIX_LIMIT = (ENV['MEDIAATTACHMENT_MAX_VIDEO_MATRIX_LIMIT'] || 8_294_400).to_i # 3840x2160px
+  MAX_VIDEO_FRAME_RATE   = (ENV['MEDIAATTACHMENT_MAX_VIDEO_FRAME_RATE'] || 120).to_i
+  MAX_VIDEO_FRAMES       = (ENV['MEDIAATTACHMENT_MAX_VIDEO_FRAMES'] || 36_000).to_i # Approx. 5 minutes at 120 fps
 
   IMAGE_FILE_EXTENSIONS = %w(.jpg .jpeg .png .gif .webp .heic .heif .avif).freeze
   VIDEO_FILE_EXTENSIONS = %w(.webm .mp4 .m4v .mov).freeze
@@ -70,7 +70,7 @@ class MediaAttachment < ApplicationRecord
 
   IMAGE_STYLES = {
     original: {
-      pixels: 8_294_400, # 3840x2160px
+      pixels: (ENV['MEDIAATTACHMENT_IMAGE_STYLES_ORIGINAL_PIXELS'] || 8_294_400).to_i, # 3840x2160px
       file_geometry_parser: FastGeometryParser,
     }.freeze,
 
