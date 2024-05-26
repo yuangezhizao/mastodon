70fb4409bd67174c93c14b0f0667397812c4e6d7
yuangezhizao-MacMini <yuangezhizao@icloud.com>
2024-05-26 17:08:14 +0800

    🔧 Config(.env.production.sample): 「媒体附件」：图片和视频大小限制提取至配置文件
    参照：https://github.com/mastodon/mastodon/pull/23726

--- app/models/concerns/attachmentable.rb
+++ app/models/concerns/attachmentable.rb
@@ -5,8 +5,8 @@ require 'mime/types/columnar'
 module Attachmentable
   extend ActiveSupport::Concern
 
-  MAX_MATRIX_LIMIT = 33_177_600 # 7680x4320px or approx. 847MB in RAM
-  GIF_MATRIX_LIMIT = 921_600    # 1280x720px
+  MAX_MATRIX_LIMIT = (ENV['ATTACHMENTABLE_MAX_MATRIX_LIMIT'] || 33_177_600).to_i # 7680x4320px or approx. 847MB in RAM
+  GIF_MATRIX_LIMIT = (ENV['ATTACHMENTABLE_GIF_MATRIX_LIMIT'] || 921_600).to_i    # 1280x720px
 
   # For some file extensions, there exist different content
   # type variants, and browsers often send the wrong one,
